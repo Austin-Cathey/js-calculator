5d98a8904d4fb8fb1ca8146aee7c64206a12c2d9
Austin-Cathey <austincatheyart@gmail.com>
2023-10-19 13:35:10 -0400

    buttons work

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const outputWindow = document.getElementById('#output');
-const number = document.querySelectorAll('.number,');
+const outputWindow = document.getElementById('output');
+const number = document.querySelectorAll('.number');
 const operator = document.querySelectorAll('.operator'); 
-const equal = document.getElementById('#equal');
-const clear = document.getElementById('#clear');
+const equal = document.getElementById('equal');
+const clear = document.getElementById('clear');
 const misMath = false;
 let numbers = [
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
 ]
 
 
-// doesn't work
+
 
 for (let button of number) {
     button.addEventListener('click', (e) => {
@@ -17,11 +17,9 @@ for (let button of number) {
     })
 };
 
-// doesn't work
-
-/* number.forEach(button => {
-    button.addEventListener('click', () => {
-        outputWindow.appendNumber(button.innerText)
-        
+for (let button of operator) {
+    button.addEventListener('click', (e) => {
+        outputWindow.textContent += e.target.textContent
     })
-}) */
+};
+
