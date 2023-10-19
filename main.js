fbab0158ebc8f6b1c3afd5f7c56e3671b4334b4f
Austin-Cathey <austincatheyart@gmail.com>
2023-10-19 17:01:25 -0400

    so close, so far

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const number = document.querySelectorAll('.number');
 const operator = document.querySelectorAll('.operator'); 
 const equal = document.getElementById('equal');
 const clear = document.getElementById('clear');
+const math = document.querySelectorAll('.operator, #equal')
 const misMath = false;
 let numbers = [
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
@@ -31,3 +32,16 @@ document.getElementById('clear').addEventListener('click', function(){
     outputWindow.textContent = outputWindow.textContent.slice(0, -1);
   }
   )
+
+//ran out of time to make work
+
+  equal.addEventListener('click', (e) => {
+    let answer = eval(outputWindow.textContent.replace());
+    outputWindow.textContent = answer;
+    misMath = true;
+  }
+  )
+ 
+  
+
+ 
